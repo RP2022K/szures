1b08c678ee89c441e167e67f4ac55ad6871db542
RP <youremail@asd.com>
2023-03-21 19:59:55 +0100

    majdnem kész

---
+++ src/app.js
@@ -0,0 +1,62 @@
+
+const doc = {
+    wordInput: document.querySelector('#word'),
+    names: document.querySelector('#name')
+}
+
+const state = {
+    nameList: []
+}
+
+window.addEventListener('load', () => {
+    init();
+    render();
+});
+
+//init();
+
+function init() {
+    state.nameList = [
+        'Erős Pista',
+        'Páros Béla',
+        'Pálma Irén',
+        'Penge Géza',
+        'Erdős Borbála',
+        'Csengő Béla'
+    ];
+    doc.wordInput.addEventListener('keyup', () => {
+        filterList();
+    })
+}
+
+function filterList() {
+    console.log(doc.wordInput.value);
+    let filteredList = state.nameList.filter(item => {
+        if (item.indexOf(doc.wordInput.value) > -1) {
+            return item;
+        }
+    });
+    doc.names.innerHTML = '';
+    let lis = '';
+    filteredList.forEach(item => {
+        console.log(item);
+        lis += `
+            <li>${item}</li>
+        `;
+    });
+    doc.names.innerHTML = lis;
+};
+
+function render() {
+    let lis = '';
+    state.nameList.forEach(name => {
+       
+        console.log(name);
+        lis += `
+            <li>${name}</li>
+        `;
+});
+
+doc.names.innerHTML = lis;
+
+}
